3be6cb37ccaed054f9436aae6f1019e5f698b335
stefanjbergh <stefanjbergh@gmail.com>
2019-04-17 12:24:14 -0500

    grab credentials from IAM role

--- centos-layer1/setup.sh
+++ centos-layer1/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo yum -y update
-sudo yum install -y docker wget git
+sudo yum install -y docker wget git unzip
 sudo systemctl enable docker && sudo systemctl start docker
-sudo docker pull stefanjbergh/duffle-docker
-#sudo docker pull <duffle+docker image>
+
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
--- centos-layer2/setup.sh
+++ centos-layer2/setup.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 sudo mv /home/centos/entrypoint /var/lib/cloud/scripts/per-boot/entrypoint
 sudo chmod 755 /var/lib/cloud/scripts/per-boot/entrypoint
+
+#sudo yum -y install unzip
+
+#curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+#unzip awscli-bundle.zip
+#sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
